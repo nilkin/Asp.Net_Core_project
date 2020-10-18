60c3407a0b73177f8da28d3b43b540a36a8fadb2
ilkin Nazarli <ilkinnazarli@gmail.com>
2020-10-18 17:22:36 +0400

    updates Admin dashboard

---
+++ CorporX/Areas/Manager/Controllers/PackagesController.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CorporX.Data;
+using CorporX.Models;
+using static CorporX.Services.FileUploader;
+using System.IO;
+
+namespace CorporX.Areas.Manager.Controllers
+{
+    [Area("Manager")]
+    public class PackagesController : Controller
+    {
+        private readonly CorporxDbContext _context;
+        private readonly IFileManager _fileManager;
+        public PackagesController(CorporxDbContext context, IFileManager fileManager)
+        {
+            _context = context;
+            _fileManager = fileManager;
+        }
+
+        // GET: Manager/Packages
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Packages.ToListAsync());
+        }
+
+        // GET: Manager/Packages/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var package = await _context.Packages
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (package == null)
+            {
+                return NotFound();
+            }
+
+            return View(package);
+        }
+
+        // GET: Manager/Packages/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Manager/Packages/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Price,Photo,BtnClass,Class,Id,Name,Content")] Package package)
+        {
+            if (package.Upload == null)
+            {
+                ModelState.AddModelError("Upload", "Photo is required");
+            }
+            else
+            {
+                if (package.Upload.ContentType != "image/jpeg" && package.Upload.ContentType != "image/png" && package.Upload.ContentType != "image/gif")
+                {
+                    ModelState.AddModelError("Upload", "You can only download png, jpg or gif file");
+                }
+
+                if (package.Upload.Length > 1048576)
+                {
+                    ModelState.AddModelError("Upload", "The file size can be a maximum of 1 MB");
+                }
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                package.Photo = _fileManager.Upload(package.Upload, "wwwroot/uploads");
+                _context.Add(package);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(package);
+        }
+
+        // GET: Manager/Packages/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var package = await _context.Packages.FindAsync(id);
+            if (package == null)
+            {
+                return NotFound();
+            }
+            return View(package);
+        }
+
+        // POST: Manager/Packages/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Price,Photo,BtnClass,Class,Id,Name,Content")] Package package)
+        {
+            if (id != package.Id)
+            {
+                return NotFound();
+            }
+            if (package.Upload == null)
+            {
+                ModelState.AddModelError("Upload", "Photo is required");
+            }
+            else
+            {
+                if (package.Upload.ContentType != "image/jpeg" && package.Upload.ContentType != "image/png"
+                 && package.Upload.ContentType != "image/gif")
+                {
+                    ModelState.AddModelError("Upload", "You can only download png, jpg or gif file");
+                }
+                if (package.Upload.Length > 1048576)
+                {
+                    ModelState.AddModelError("Upload", "The file size can be a maximum of 1 MB");
+                }
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+                    _fileManager.Delete(package.Photo);
+                    package.Photo = _fileManager.Upload(package.Upload);
+                    _context.Update(package).State = EntityState.Modified;
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PackageExists(package.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(package);
+        }
+
+        // GET: Manager/Packages/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var package = await _context.Packages
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (package == null)
+            {
+                return NotFound();
+            }
+
+            return View(package);
+        }
+
+        // POST: Manager/Packages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var package = await _context.Packages.FindAsync(id);
+            try
+            {
+                _fileManager.Delete(package.Photo);
+            }
+            catch (FileNotFoundException)
+            {
+                _context.Packages.Remove(package);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            _context.Packages.Remove(package);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PackageExists(int id)
+        {
+            return _context.Packages.Any(e => e.Id == id);
+        }
+    }
+}
